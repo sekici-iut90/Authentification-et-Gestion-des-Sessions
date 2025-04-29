b60684cadb10ae73da31dd9ed9629e724935cb7f
lmerat-iut90 <leslie.merat@edu.univ-fcomte.fr>
2025-04-29 19:29:26 +0200

    Réorganisation des projets 2

--- projet1/S4_MONGOOSE_OAUTH/config/dev.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-    googleClientID: process.env.GOOGLE_CLIENT_ID,
-    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: "mongodb://localhost:27017/<id>",
-  cookieKey: "BUTINFOS5"
-};
--- projet1/S4_MONGOOSE_OAUTH/models/User.js
+++
@@ -1,9 +0,0 @@
-//TODO
-const mongoose =require('mongoose');
-const {Schema} = mongoose;
-
-const userSchema = new Schema({//collection
-    googleId: String,
-    displayName: String
-});
-mongoose.model("User", userSchema);
